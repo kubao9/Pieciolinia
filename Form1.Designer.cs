4658ca9f9249e39cb48454c61e2392a24ef755e5
galileo680 <bartek.wicz@gmail.com>
2023-12-09 21:26:42 +0100

    Obsługa dzwięku klawiszy

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,237 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.c = new System.Windows.Forms.Button();
-            this.d = new System.Windows.Forms.Button();
-            this.e = new System.Windows.Forms.Button();
-            this.f = new System.Windows.Forms.Button();
-            this.g = new System.Windows.Forms.Button();
-            this.a = new System.Windows.Forms.Button();
-            this.h = new System.Windows.Forms.Button();
-            this.cis = new System.Windows.Forms.Button();
-            this.dis = new System.Windows.Forms.Button();
-            this.fis = new System.Windows.Forms.Button();
-            this.gis = new System.Windows.Forms.Button();
-            this.b = new System.Windows.Forms.Button();
-            this.options = new System.Windows.Forms.GroupBox();
-            this.recordButton = new System.Windows.Forms.Button();
-            this.loadButton = new System.Windows.Forms.Button();
-            this.saveButton = new System.Windows.Forms.Button();
-            this.playButton = new System.Windows.Forms.Button();
-            this.options.SuspendLayout();
-            this.SuspendLayout();
+            c = new Button();
+            d = new Button();
+            e = new Button();
+            f = new Button();
+            g = new Button();
+            a = new Button();
+            h = new Button();
+            cis = new Button();
+            dis = new Button();
+            fis = new Button();
+            gis = new Button();
+            b = new Button();
+            options = new GroupBox();
+            recordButton = new Button();
+            loadButton = new Button();
+            saveButton = new Button();
+            playButton = new Button();
+            options.SuspendLayout();
+            SuspendLayout();
             // 
             // c
             // 
-            this.c.BackColor = System.Drawing.Color.White;
-            this.c.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.c.Location = new System.Drawing.Point(1, 0);
-            this.c.Name = "c";
-            this.c.Size = new System.Drawing.Size(94, 449);
-            this.c.TabIndex = 0;
-            this.c.Text = "C";
-            this.c.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.c.UseVisualStyleBackColor = false;
+            c.BackColor = Color.White;
+            c.Font = new Font("Segoe UI", 19.8F, FontStyle.Regular, GraphicsUnit.Point);
+            c.Location = new Point(1, 0);
+            c.Margin = new Padding(3, 2, 3, 2);
+            c.Name = "c";
+            c.Size = new Size(82, 337);
+            c.TabIndex = 0;
+            c.Text = "C";
+            c.TextAlign = ContentAlignment.BottomCenter;
+            c.UseVisualStyleBackColor = false;
+            c.Click += c_Click;
             // 
             // d
             // 
-            this.d.BackColor = System.Drawing.Color.White;
-            this.d.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.d.Location = new System.Drawing.Point(91, 0);
-            this.d.Name = "d";
-            this.d.Size = new System.Drawing.Size(94, 449);
-            this.d.TabIndex = 1;
-            this.d.Text = "D";
-            this.d.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.d.UseVisualStyleBackColor = false;
+            d.BackColor = Color.White;
+            d.Font = new Font("Segoe UI", 19.8F, FontStyle.Regular, GraphicsUnit.Point);
+            d.Location = new Point(80, 0);
+            d.Margin = new Padding(3, 2, 3, 2);
+            d.Name = "d";
+            d.Size = new Size(82, 337);
+            d.TabIndex = 1;
+            d.Text = "D";
+            d.TextAlign = ContentAlignment.BottomCenter;
+            d.UseVisualStyleBackColor = false;
+            d.Click += d_Click;
             // 
             // e
             // 
-            this.e.BackColor = System.Drawing.Color.White;
-            this.e.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.e.Location = new System.Drawing.Point(182, 0);
-            this.e.Name = "e";
-            this.e.Size = new System.Drawing.Size(94, 449);
-            this.e.TabIndex = 2;
-            this.e.Text = "E";
-            this.e.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.e.UseVisualStyleBackColor = false;
+            e.BackColor = Color.White;
+            e.Font = new Font("Segoe UI", 19.8F, FontStyle.Regular, GraphicsUnit.Point);
+            e.Location = new Point(159, 0);
+            e.Margin = new Padding(3, 2, 3, 2);
+            e.Name = "e";
+            e.Size = new Size(82, 337);
+            e.TabIndex = 2;
+            e.Text = "E";
+            e.TextAlign = ContentAlignment.BottomCenter;
+            e.UseVisualStyleBackColor = false;
+            e.Click += e_Click;
             // 
             // f
             // 
-            this.f.BackColor = System.Drawing.Color.White;
-            this.f.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.f.Location = new System.Drawing.Point(272, 0);
-            this.f.Name = "f";
-            this.f.Size = new System.Drawing.Size(94, 449);
-            this.f.TabIndex = 3;
-            this.f.Text = "F";
-            this.f.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.f.UseVisualStyleBackColor = false;
+            f.BackColor = Color.White;
+            f.Font = new Font("Segoe UI", 19.8F, FontStyle.Regular, GraphicsUnit.Point);
+            f.Location = new Point(238, 0);
+            f.Margin = new Padding(3, 2, 3, 2);
+            f.Name = "f";
+            f.Size = new Size(82, 337);
+            f.TabIndex = 3;
+            f.Text = "F";
+            f.TextAlign = ContentAlignment.BottomCenter;
+            f.UseVisualStyleBackColor = false;
+            f.Click += f_Click;
             // 
             // g
             // 
-            this.g.BackColor = System.Drawing.Color.White;
-            this.g.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.g.Location = new System.Drawing.Point(363, 0);
-            this.g.Name = "g";
-            this.g.Size = new System.Drawing.Size(94, 449);
-            this.g.TabIndex = 4;
-            this.g.Text = "G";
-            this.g.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.g.UseVisualStyleBackColor = false;
+            g.BackColor = Color.White;
+            g.Font = new Font("Segoe UI", 19.8F, FontStyle.Regular, GraphicsUnit.Point);
+            g.Location = new Point(318, 0);
+            g.Margin = new Padding(3, 2, 3, 2);
+            g.Name = "g";
+            g.Size = new Size(82, 337);
+            g.TabIndex = 4;
+            g.Text = "G";
+            g.TextAlign = ContentAlignment.BottomCenter;
+            g.UseVisualStyleBackColor = false;
+            g.Click += g_Click;
             // 
             // a
             // 
-            this.a.BackColor = System.Drawing.Color.White;
-            this.a.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.a.Location = new System.Drawing.Point(454, 0);
-            this.a.Name = "a";
-            this.a.Size = new System.Drawing.Size(94, 449);
-            this.a.TabIndex = 5;
-            this.a.Text = "A";
-            this.a.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.a.UseVisualStyleBackColor = false;
+            a.BackColor = Color.White;
+            a.Font = new Font("Segoe UI", 19.8F, FontStyle.Regular, GraphicsUnit.Point);
+            a.Location = new Point(397, 0);
+            a.Margin = new Padding(3, 2, 3, 2);
+            a.Name = "a";
+            a.Size = new Size(82, 337);
+            a.TabIndex = 5;
+            a.Text = "A";
+            a.TextAlign = ContentAlignment.BottomCenter;
+            a.UseVisualStyleBackColor = false;
+            a.Click += a_Click;
             // 
             // h
             // 
-            this.h.BackColor = System.Drawing.Color.White;
-            this.h.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.h.Location = new System.Drawing.Point(544, 0);
-            this.h.Name = "h";
-            this.h.Size = new System.Drawing.Size(94, 449);
-            this.h.TabIndex = 6;
-            this.h.Text = "H";
-            this.h.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.h.UseVisualStyleBackColor = false;
+            h.BackColor = Color.White;
+            h.Font = new Font("Segoe UI", 19.8F, FontStyle.Regular, GraphicsUnit.Point);
+            h.Location = new Point(476, 0);
+            h.Margin = new Padding(3, 2, 3, 2);
+            h.Name = "h";
+            h.Size = new Size(82, 337);
+            h.TabIndex = 6;
+            h.Text = "H";
+            h.TextAlign = ContentAlignment.BottomCenter;
+            h.UseVisualStyleBackColor = false;
+            h.Click += h_Click;
             // 
             // cis
             // 
-            this.cis.BackColor = System.Drawing.Color.Black;
-            this.cis.ForeColor = System.Drawing.Color.White;
-            this.cis.Location = new System.Drawing.Point(63, 0);
-            this.cis.Name = "cis";
-            this.cis.Size = new System.Drawing.Size(59, 312);
-            this.cis.TabIndex = 7;
-            this.cis.Text = "CIS DES";
-            this.cis.UseVisualStyleBackColor = false;
+            cis.BackColor = Color.Black;
+            cis.ForeColor = Color.White;
+            cis.Location = new Point(55, 0);
+            cis.Margin = new Padding(3, 2, 3, 2);
+            cis.Name = "cis";
+            cis.Size = new Size(52, 234);
+            cis.TabIndex = 7;
+            cis.Text = "CIS DES";
+            cis.UseVisualStyleBackColor = false;
+            cis.Click += cis_Click;
             // 
             // dis
             // 
-            this.dis.BackColor = System.Drawing.Color.Black;
-            this.dis.ForeColor = System.Drawing.Color.White;
-            this.dis.Location = new System.Drawing.Point(153, 0);
-            this.dis.Name = "dis";
-            this.dis.Size = new System.Drawing.Size(59, 312);
-            this.dis.TabIndex = 8;
-            this.dis.Text = "DIS  ES";
-            this.dis.UseVisualStyleBackColor = false;
+            dis.BackColor = Color.Black;
+            dis.ForeColor = Color.White;
+            dis.Location = new Point(134, 0);
+            dis.Margin = new Padding(3, 2, 3, 2);
+            dis.Name = "dis";
+            dis.Size = new Size(52, 234);
+            dis.TabIndex = 8;
+            dis.Text = "DIS  ES";
+            dis.UseVisualStyleBackColor = false;
+            dis.Click += dis_Click;
             // 
             // fis
             // 
-            this.fis.BackColor = System.Drawing.Color.Black;
-            this.fis.ForeColor = System.Drawing.Color.White;
-            this.fis.Location = new System.Drawing.Point(334, 0);
-            this.fis.Name = "fis";
-            this.fis.Size = new System.Drawing.Size(59, 312);
-            this.fis.TabIndex = 9;
-            this.fis.Text = "FIS GES";
-            this.fis.UseVisualStyleBackColor = false;
+            fis.BackColor = Color.Black;
+            fis.ForeColor = Color.White;
+            fis.Location = new Point(292, 0);
+            fis.Margin = new Padding(3, 2, 3, 2);
+            fis.Name = "fis";
+            fis.Size = new Size(52, 234);
+            fis.TabIndex = 9;
+            fis.Text = "FIS GES";
+            fis.UseVisualStyleBackColor = false;
+            fis.Click += fis_Click;
             // 
             // gis
             // 
-            this.gis.BackColor = System.Drawing.Color.Black;
-            this.gis.ForeColor = System.Drawing.Color.White;
-            this.gis.Location = new System.Drawing.Point(427, 0);
-            this.gis.Name = "gis";
-            this.gis.Size = new System.Drawing.Size(59, 312);
-            this.gis.TabIndex = 10;
-            this.gis.Text = "GIS  AS";
-            this.gis.UseVisualStyleBackColor = false;
+            gis.BackColor = Color.Black;
+            gis.ForeColor = Color.White;
+            gis.Location = new Point(374, 0);
+            gis.Margin = new Padding(3, 2, 3, 2);
+            gis.Name = "gis";
+            gis.Size = new Size(52, 234);
+            gis.TabIndex = 10;
+            gis.Text = "GIS  AS";
+            gis.UseVisualStyleBackColor = false;
+            gis.Click += gis_Click;
             // 
             // b
             // 
-            this.b.BackColor = System.Drawing.Color.Black;
-            this.b.ForeColor = System.Drawing.Color.White;
-            this.b.Location = new System.Drawing.Point(517, 0);
-            this.b.Name = "b";
-            this.b.Size = new System.Drawing.Size(59, 312);
-            this.b.TabIndex = 11;
-            this.b.Text = " AIS   B";
-            this.b.UseVisualStyleBackColor = false;
+            b.BackColor = Color.Black;
+            b.ForeColor = Color.White;
+            b.Location = new Point(452, 0);
+            b.Margin = new Padding(3, 2, 3, 2);
+            b.Name = "b";
+            b.Size = new Size(52, 234);
+            b.TabIndex = 11;
+            b.Text = " AIS   B";
+            b.UseVisualStyleBackColor = false;
+            b.Click += b_Click;
             // 
             // options
             // 
-            this.options.Controls.Add(this.recordButton);
-            this.options.Controls.Add(this.loadButton);
-            this.options.Controls.Add(this.saveButton);
-            this.options.Controls.Add(this.playButton);
-            this.options.Location = new System.Drawing.Point(644, 0);
-            this.options.Name = "options";
-            this.options.Size = new System.Drawing.Size(144, 438);
-            this.options.TabIndex = 12;
-            this.options.TabStop = false;
-            this.options.Text = "OPCJE";
+            options.Controls.Add(recordButton);
+            options.Controls.Add(loadButton);
+            options.Controls.Add(saveButton);
+            options.Controls.Add(playButton);
+            options.Location = new Point(564, 0);
+            options.Margin = new Padding(3, 2, 3, 2);
+            options.Name = "options";
+            options.Padding = new Padding(3, 2, 3, 2);
+            options.Size = new Size(126, 328);
+            options.TabIndex = 12;
+            options.TabStop = false;
+            options.Text = "OPCJE";
             // 
             // recordButton
             // 
-            this.recordButton.Location = new System.Drawing.Point(6, 117);
-            this.recordButton.Name = "recordButton";
-            this.recordButton.Size = new System.Drawing.Size(132, 85);
-            this.recordButton.TabIndex = 3;
-            this.recordButton.Text = "REJESTRUJ";
-            this.recordButton.UseVisualStyleBackColor = true;
+            recordButton.Location = new Point(5, 88);
+            recordButton.Margin = new Padding(3, 2, 3, 2);
+            recordButton.Name = "recordButton";
+            recordButton.Size = new Size(116, 64);
+            recordButton.TabIndex = 3;
+            recordButton.Text = "REJESTRUJ";
+            recordButton.UseVisualStyleBackColor = true;
             // 
             // loadButton
             // 
-            this.loadButton.Location = new System.Drawing.Point(6, 208);
-            this.loadButton.Name = "loadButton";
-            this.loadButton.Size = new System.Drawing.Size(132, 85);
-            this.loadButton.TabIndex = 2;
-            this.loadButton.Text = "WCZYTAJ";
-            this.loadButton.UseVisualStyleBackColor = true;
+            loadButton.Location = new Point(5, 156);
+            loadButton.Margin = new Padding(3, 2, 3, 2);
+            loadButton.Name = "loadButton";
+            loadButton.Size = new Size(116, 64);
+            loadButton.TabIndex = 2;
+            loadButton.Text = "WCZYTAJ";
+            loadButton.UseVisualStyleBackColor = true;
             // 
             // saveButton
             // 
-            this.saveButton.Location = new System.Drawing.Point(6, 299);
-            this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(132, 85);
-            this.saveButton.TabIndex = 1;
-            this.saveButton.Text = "ZAPISZ";
-            this.saveButton.UseVisualStyleBackColor = true;
+            saveButton.Location = new Point(5, 224);
+            saveButton.Margin = new Padding(3, 2, 3, 2);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(116, 64);
+            saveButton.TabIndex = 1;
+            saveButton.Text = "ZAPISZ";
+            saveButton.UseVisualStyleBackColor = true;
             // 
             // playButton
             // 
-            this.playButton.Location = new System.Drawing.Point(6, 26);
-            this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(132, 85);
-            this.playButton.TabIndex = 0;
-            this.playButton.Text = "GRAJ";
-            this.playButton.UseVisualStyleBackColor = true;
+            playButton.Location = new Point(5, 20);
+            playButton.Margin = new Padding(3, 2, 3, 2);
+            playButton.Name = "playButton";
+            playButton.Size = new Size(116, 64);
+            playButton.TabIndex = 0;
+            playButton.Text = "GRAJ";
+            playButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.options);
-            this.Controls.Add(this.b);
-            this.Controls.Add(this.gis);
-            this.Controls.Add(this.fis);
-            this.Controls.Add(this.dis);
-            this.Controls.Add(this.cis);
-            this.Controls.Add(this.h);
-            this.Controls.Add(this.a);
-            this.Controls.Add(this.g);
-            this.Controls.Add(this.f);
-            this.Controls.Add(this.e);
-            this.Controls.Add(this.d);
-            this.Controls.Add(this.c);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.options.ResumeLayout(false);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 338);
+            Controls.Add(options);
+            Controls.Add(b);
+            Controls.Add(gis);
+            Controls.Add(fis);
+            Controls.Add(dis);
+            Controls.Add(cis);
+            Controls.Add(h);
+            Controls.Add(a);
+            Controls.Add(g);
+            Controls.Add(f);
+            Controls.Add(e);
+            Controls.Add(d);
+            Controls.Add(c);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            options.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,73 @@ namespace Pieciolinia
         {
             InitializeComponent();
         }
+
+        public void Play(string notePath)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(notePath);
+            player.Play();
+        }
+
+        private void c_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/c1.wav");
+        }
+
+        private void cis_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/csharp1.wav");
+        }
+
+        private void d_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/d1.wav");
+        }
+
+        private void dis_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/dsharp1.wav");
+        }
+
+        private void e_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/e1.wav");
+        }
+
+        private void f_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/f1.wav");
+        }
+
+        private void fis_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/fsharp1.wav");
+        }
+
+        private void g_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/g1.wav");
+        }
+
+        private void gis_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/gsharp1.wav");
+        }
+
+        private void a_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/a1.wav");
+        }
+
+        private void b_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/asharp1.wav");
+        }
+
+        private void h_Click(object sender, EventArgs e)
+        {
+            Play(@"NotesAudio/b1.wav");
+        }
+
+
     }
 }
