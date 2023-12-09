9eb605058c5a07c6193dacf776d43477a1628ec5
kubao9 <kuba2925@gmail.com>
2023-12-09 11:03:00 +0100

    interfejs użytkownika w Win Forms

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Pieciolinia
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.c = new System.Windows.Forms.Button();
+            this.d = new System.Windows.Forms.Button();
+            this.e = new System.Windows.Forms.Button();
+            this.f = new System.Windows.Forms.Button();
+            this.g = new System.Windows.Forms.Button();
+            this.a = new System.Windows.Forms.Button();
+            this.h = new System.Windows.Forms.Button();
+            this.cis = new System.Windows.Forms.Button();
+            this.dis = new System.Windows.Forms.Button();
+            this.fis = new System.Windows.Forms.Button();
+            this.gis = new System.Windows.Forms.Button();
+            this.b = new System.Windows.Forms.Button();
+            this.options = new System.Windows.Forms.GroupBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.recordButton = new System.Windows.Forms.Button();
+            this.options.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // c
+            // 
+            this.c.BackColor = System.Drawing.Color.White;
+            this.c.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.c.Location = new System.Drawing.Point(1, 0);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(94, 449);
+            this.c.TabIndex = 0;
+            this.c.Text = "C";
+            this.c.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.c.UseVisualStyleBackColor = false;
+            // 
+            // d
+            // 
+            this.d.BackColor = System.Drawing.Color.White;
+            this.d.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.d.Location = new System.Drawing.Point(91, 0);
+            this.d.Name = "d";
+            this.d.Size = new System.Drawing.Size(94, 449);
+            this.d.TabIndex = 1;
+            this.d.Text = "D";
+            this.d.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.d.UseVisualStyleBackColor = false;
+            // 
+            // e
+            // 
+            this.e.BackColor = System.Drawing.Color.White;
+            this.e.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.e.Location = new System.Drawing.Point(182, 0);
+            this.e.Name = "e";
+            this.e.Size = new System.Drawing.Size(94, 449);
+            this.e.TabIndex = 2;
+            this.e.Text = "E";
+            this.e.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.e.UseVisualStyleBackColor = false;
+            // 
+            // f
+            // 
+            this.f.BackColor = System.Drawing.Color.White;
+            this.f.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.f.Location = new System.Drawing.Point(272, 0);
+            this.f.Name = "f";
+            this.f.Size = new System.Drawing.Size(94, 449);
+            this.f.TabIndex = 3;
+            this.f.Text = "F";
+            this.f.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.f.UseVisualStyleBackColor = false;
+            // 
+            // g
+            // 
+            this.g.BackColor = System.Drawing.Color.White;
+            this.g.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.g.Location = new System.Drawing.Point(363, 0);
+            this.g.Name = "g";
+            this.g.Size = new System.Drawing.Size(94, 449);
+            this.g.TabIndex = 4;
+            this.g.Text = "G";
+            this.g.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.g.UseVisualStyleBackColor = false;
+            // 
+            // a
+            // 
+            this.a.BackColor = System.Drawing.Color.White;
+            this.a.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.a.Location = new System.Drawing.Point(454, 0);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(94, 449);
+            this.a.TabIndex = 5;
+            this.a.Text = "A";
+            this.a.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.a.UseVisualStyleBackColor = false;
+            // 
+            // h
+            // 
+            this.h.BackColor = System.Drawing.Color.White;
+            this.h.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.h.Location = new System.Drawing.Point(544, 0);
+            this.h.Name = "h";
+            this.h.Size = new System.Drawing.Size(94, 449);
+            this.h.TabIndex = 6;
+            this.h.Text = "H";
+            this.h.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.h.UseVisualStyleBackColor = false;
+            // 
+            // cis
+            // 
+            this.cis.BackColor = System.Drawing.Color.Black;
+            this.cis.ForeColor = System.Drawing.Color.White;
+            this.cis.Location = new System.Drawing.Point(63, 0);
+            this.cis.Name = "cis";
+            this.cis.Size = new System.Drawing.Size(59, 312);
+            this.cis.TabIndex = 7;
+            this.cis.Text = "CIS";
+            this.cis.UseVisualStyleBackColor = false;
+            // 
+            // dis
+            // 
+            this.dis.BackColor = System.Drawing.Color.Black;
+            this.dis.ForeColor = System.Drawing.Color.White;
+            this.dis.Location = new System.Drawing.Point(153, 0);
+            this.dis.Name = "dis";
+            this.dis.Size = new System.Drawing.Size(59, 312);
+            this.dis.TabIndex = 8;
+            this.dis.Text = "DIS";
+            this.dis.UseVisualStyleBackColor = false;
+            // 
+            // fis
+            // 
+            this.fis.BackColor = System.Drawing.Color.Black;
+            this.fis.ForeColor = System.Drawing.Color.White;
+            this.fis.Location = new System.Drawing.Point(334, 0);
+            this.fis.Name = "fis";
+            this.fis.Size = new System.Drawing.Size(59, 312);
+            this.fis.TabIndex = 9;
+            this.fis.Text = "FIS";
+            this.fis.UseVisualStyleBackColor = false;
+            // 
+            // gis
+            // 
+            this.gis.BackColor = System.Drawing.Color.Black;
+            this.gis.ForeColor = System.Drawing.Color.White;
+            this.gis.Location = new System.Drawing.Point(427, 0);
+            this.gis.Name = "gis";
+            this.gis.Size = new System.Drawing.Size(59, 312);
+            this.gis.TabIndex = 10;
+            this.gis.Text = "GIS";
+            this.gis.UseVisualStyleBackColor = false;
+            // 
+            // b
+            // 
+            this.b.BackColor = System.Drawing.Color.Black;
+            this.b.ForeColor = System.Drawing.Color.White;
+            this.b.Location = new System.Drawing.Point(517, 0);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(59, 312);
+            this.b.TabIndex = 11;
+            this.b.Text = "B";
+            this.b.UseVisualStyleBackColor = false;
+            // 
+            // options
+            // 
+            this.options.Controls.Add(this.recordButton);
+            this.options.Controls.Add(this.loadButton);
+            this.options.Controls.Add(this.saveButton);
+            this.options.Controls.Add(this.playButton);
+            this.options.Location = new System.Drawing.Point(644, 0);
+            this.options.Name = "options";
+            this.options.Size = new System.Drawing.Size(144, 438);
+            this.options.TabIndex = 12;
+            this.options.TabStop = false;
+            this.options.Text = "OPCJE";
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(6, 26);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(132, 85);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "GRAJ";
+            this.playButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(6, 299);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(132, 85);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "ZAPISZ";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(6, 208);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(132, 85);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "WCZYTAJ";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // recordButton
+            // 
+            this.recordButton.Location = new System.Drawing.Point(6, 117);
+            this.recordButton.Name = "recordButton";
+            this.recordButton.Size = new System.Drawing.Size(132, 85);
+            this.recordButton.TabIndex = 3;
+            this.recordButton.Text = "REJESTRUJ";
+            this.recordButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.options);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.gis);
+            this.Controls.Add(this.fis);
+            this.Controls.Add(this.dis);
+            this.Controls.Add(this.cis);
+            this.Controls.Add(this.h);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.g);
+            this.Controls.Add(this.f);
+            this.Controls.Add(this.e);
+            this.Controls.Add(this.d);
+            this.Controls.Add(this.c);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.options.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button c;
+        private Button d;
+        private Button e;
+        private Button f;
+        private Button g;
+        private Button a;
+        private Button h;
+        private Button cis;
+        private Button dis;
+        private Button fis;
+        private Button gis;
+        private Button b;
+        private GroupBox options;
+        private Button recordButton;
+        private Button loadButton;
+        private Button saveButton;
+        private Button playButton;
+    }
+}
